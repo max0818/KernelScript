09b3f369eb81df5762b72f87dd58079c06c9b5e0
max0818 <maxam0818@gmail.com>
2026-09-05 15:56:06 +0700

    Fix parser exp

--- js/parser/tools/expressions.js
+++ js/parser/tools/expressions.js
@@ -274,6 +274,11 @@ class ParserExpressions extends ParserBase {
 			return this.parseNew();
 		}
 
+		if (peek.type === ';') {
+			ParserPosManager.pos++;
+			return {type: 'Separator'};
+		}
+
 		this.error(`Неожиданный токен на строке ${peek.row}: ${peek.value} (${peek.type})`);
 	}
 
