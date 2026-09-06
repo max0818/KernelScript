945ee3fa1f1c0e90dbb47071f31ab09206873d47
max0818 <maxam0818@gmail.com>
2026-09-07 04:53:57 +0700

    Fix parser cls

--- js/parser/tools/classes.js
+++ js/parser/tools/classes.js
@@ -14,7 +14,40 @@ class ParserClasses extends ParserBase {
 	parseClassConstructor() {}
 
 	// Поле
-	parseClassField() {}
+	parseClassField() {
+		let isPrivate = false;
+		let isStatic = false;
+
+		if (this.peek()?.type === 'private') {
+			isPrivate = true;
+			ParserPosManager.pos++;
+		}
+
+		if (this.peek()?.type === 'static') {
+			isStatic = true;
+			ParserPosManager.pos++;
+		}
+
+		this.expectType('identifier');
+		const key = this.peek().value;
+		ParserPosManager.pos++;
+
+		let value = null;
+		if (this.peek()?.type === '=') {
+			ParserPosManager.pos++;
+			value = new ParserExpressions().parseExpression();
+		}
+
+		if (this.peek()?.type === ';') ParserPosManager.pos++;
+
+		return {
+			type: 'ClassProperty',
+			key,
+			value,
+			isPrivate,
+			isStatic
+		};
+	}
 
 	// Метод
 	parseClassMethod() {}
